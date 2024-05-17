00178a23dcb6a3924111765f3964f83ee23310ce
Chrilleweb <christian.munknissen@gmail.com>
2024-05-17 23:35:26 +0200

    Prettier + color changes

--- frontend/src/components/backArrow.svelte
+++ frontend/src/components/backArrow.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-    export let route: string;
-  </script>
-  
-  <a href={route}>
-    <img class="mt-4" src="/svg/backArrow.svg" alt="Back" height="40" width="40" />
-  </a>
-  
+	export let route: string;
+</script>
+
+<a href={route}>
+	<img class="mt-4" src="/svg/backArrow.svg" alt="Back" height="40" width="40" />
+</a>
--- frontend/src/routes/user-details/+page.svelte
+++ frontend/src/routes/user-details/+page.svelte
@@ -72,42 +72,53 @@
 </svelte:head>
 
 {#if userId && isAuthenticated}
-    <div class="bg-gradient-to-r from-gray-500 to-blue-700 p-6 rounded-t-lg flex justify-between items-center">
-      <h1 class="text-4xl font-extrabold text-white">User Profile</h1>
-      <img src="/svg/user-details.svg" alt="User Icon" class="w-12 h-12"/>
-    </div>
-    <div class="bg-gradient-to-r from-gray-500 to-blue-700 p-6 rounded-b-lg space-y-6">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div class="space-y-2">
-          <p class="text-lg text-gray-800"><span class="font-bold text-white">Username:</span> {username}</p>
-          <p class="text-lg text-gray-800"><span class="font-bold text-white">Email:</span> {userEmail}</p>
-          <p class="text-lg text-gray-800"><span class="font-bold text-white">Role:</span> {userRole}</p>
-        </div>
-        </div>
-        <div class="flex justify-center md:justify-end space-x-2 mt-4 md:mt-0">
-            <button
-              on:click={() => askDeleteConfirmation(userId)}
-              class="bg-red-700 text-white px-3 py-1 rounded-md hover:bg-red-600 transition duration-150 flex items-center space-x-1"
-            >
-              <img src="/svg/delete-icon.svg" alt="Delete Icon" class="w-4 h-4"/>
-              <span class="sm:text-base text-xs">Delete User</span>
-            </button>
-            <button
-              on:click={() => goto('/request-reset')}
-              class="bg-gray-700 text-white px-3 py-1 rounded-md hover:bg-gray-600 transition duration-150 flex items-center space-x-1"
-            >
-              <img src="/svg/reset-icon.svg" alt="Reset Icon" class="w-4 h-4"/>
-              <span class="sm:text-base text-xs">Request Password Reset</span>
-            </button>
-          </div> 
-    </div>
-    <BackArrow route="/home" />
-  <Confirmation
-    isOpen={showConfirmation}
-    message="Are you sure you want to delete {username}?"
-    onConfirm={deleteUser}
-    onCancel={cancelDelete}
-  />
+	<div
+		class="bg-gradient-to-r from-gray-500 to-blue-900 p-6 rounded-t-lg flex justify-between items-center"
+	>
+		<h1 class="text-4xl font-extrabold text-white">User Profile</h1>
+		<img src="/svg/user-details.svg" alt="User Icon" class="w-12 h-12" />
+	</div>
+	<div class="bg-gradient-to-r from-gray-500 to-blue-900 p-6 rounded-b-lg space-y-6">
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+			<div class="space-y-2">
+				<p class="text-lg text-gray-800">
+					<span class="font-bold text-white">Username:</span>
+					{username}
+				</p>
+				<p class="text-lg text-gray-800">
+					<span class="font-bold text-white">Email:</span>
+					{userEmail}
+				</p>
+				<p class="text-lg text-gray-800">
+					<span class="font-bold text-white">Role:</span>
+					{userRole}
+				</p>
+			</div>
+		</div>
+		<div class="flex justify-center md:justify-end space-x-2 mt-4 md:mt-0">
+			<button
+				on:click={() => askDeleteConfirmation(userId)}
+				class="bg-red-700 text-white px-3 py-1 rounded-md hover:bg-red-600 transition duration-150 flex items-center space-x-1"
+			>
+				<img src="/svg/delete-icon.svg" alt="Delete Icon" class="w-4 h-4" />
+				<span class="sm:text-base text-xs">Delete User</span>
+			</button>
+			<button
+				on:click={() => goto('/request-reset')}
+				class="bg-gray-700 text-white px-3 py-1 rounded-md hover:bg-gray-600 transition duration-150 flex items-center space-x-1"
+			>
+				<img src="/svg/reset-icon.svg" alt="Reset Icon" class="w-4 h-4" />
+				<span class="sm:text-base text-xs">Request Password Reset</span>
+			</button>
+		</div>
+	</div>
+	<BackArrow route="/home" />
+	<Confirmation
+		isOpen={showConfirmation}
+		message="Are you sure you want to delete {username}?"
+		onConfirm={deleteUser}
+		onCancel={cancelDelete}
+	/>
 {:else}
-  <ErrorAuth />
+	<ErrorAuth />
 {/if}
