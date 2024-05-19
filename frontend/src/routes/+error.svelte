7c27860b3b120763c862e9ff4b71ae95012b5d29
Chrilleweb <christian.munknissen@gmail.com>
2024-05-19 12:30:40 +0200

    Added error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<div class="container mx-auto py-8 text-center">
+	<h2 class="text-3xl mb-4">Page Not Found</h2>
+	<p class="text-lg text-gray-700 leading-relaxed">
+		Oops! The page you are looking for does not exist or another error occurred. Go back to the
+		<a href="/" class="text-blue-500 hover:underline">homepage</a>
+	</p>
+</div>
