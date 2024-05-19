abb2a8d90a501c04f0597a45e6a6487e048e07ec
Chrilleweb <christian.munknissen@gmail.com>
2024-05-19 15:08:48 +0200

    Update admin page styling and table layout

--- frontend/src/routes/admin/+page.svelte
+++ frontend/src/routes/admin/+page.svelte
@@ -79,56 +79,65 @@
 </svelte:head>
 
 {#if userId && userRole === 'admin' && isAuthenticated}
-	<div class="container mx-auto py-8">
-		<h1 class="text-3xl text-center mb-4">Admin page</h1>
-		<p class="text-lg text-gray-700 leading-relaxed">Welcome, {userName}!</p>
-		<table class="min-w-full leading-normal mt-4">
-			<thead>
-				<tr>
-					<th
-						class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-						>Username</th
-					>
-					<th
-						class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-						>Email</th
-					>
-					<th
-						class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-						>Role</th
-					>
-					<th
-						class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-						>Actions</th
-					>
-				</tr>
-			</thead>
-			<tbody>
-				{#each dataUsers as user}
+	<div
+		class="container mx-auto py-10 px-6 bg-gradient-to-r from-gray-900 to-green-700 text-white rounded-3xl shadow-lg opacity-80"
+	>
+		<h1 class="text-4xl text-center mb-6 font-extrabold">Admin Page</h1>
+		<div class="overflow-auto rounded-lg shadow-md">
+			<table class="min-w-full leading-normal">
+				<thead>
 					<tr>
-						<td class="px-5 py-2 border-b border-gray-200 bg-white text-sm">{user.username}</td>
-						<td class="px-5 py-2 border-b border-gray-200 bg-white text-sm">{user.email}</td>
-						<td class="px-5 py-2 border-b border-gray-200 bg-white text-sm">{user.role}</td>
-						<td class="px-5 py-2 border-b border-gray-200 bg-white text-sm">
-							<button
-								on:click={() => askDeleteConfirmation(user.id)}
-								class="text-red-500 hover:text-red-700">Delete</button
-							>
-							<select
-								bind:value={user.role}
-								on:change={(event) => handleRoleChange(event, user.id)}
-								class="block w-full mt-1 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-							>
-								<option value="admin">Admin</option>
-								<option value="user">User</option>
-							</select>
-						</td>
+						<th
+							class="px-5 py-3 border-b-2 border-gray-600 bg-gray-800 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider"
+							>Username</th
+						>
+						<th
+							class="px-5 py-3 border-b-2 border-gray-600 bg-gray-800 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider"
+							>Email</th
+						>
+						<th
+							class="px-5 py-3 border-b-2 border-gray-600 bg-gray-800 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider"
+							>Role</th
+						>
+						<th
+							class="px-5 py-3 border-b-2 border-gray-600 bg-gray-800 text-left text-xs font-semibold text-gray-300 uppercase tracking-wider"
+							>Actions</th
+						>
 					</tr>
-				{/each}
-			</tbody>
-		</table>
-		<BackArrow />
+				</thead>
+				<tbody>
+					{#each dataUsers as user}
+						<tr>
+							<td class="px-5 py-2 border-b border-gray-700 bg-gray-800 text-sm text-gray-300"
+								>{user.username}</td
+							>
+							<td class="px-5 py-2 border-b border-gray-700 bg-gray-800 text-sm text-gray-300"
+								>{user.email}</td
+							>
+							<td class="px-5 py-2 border-b border-gray-700 bg-gray-800 text-sm text-gray-300"
+								>{user.role}</td
+							>
+							<td class="px-5 py-2 border-b border-gray-700 bg-gray-800 text-sm text-gray-300">
+								<button
+									on:click={() => askDeleteConfirmation(user.id)}
+									class="text-red-400 hover:text-red-600 transition duration-150">Delete</button
+								>
+								<select
+									bind:value={user.role}
+									on:change={(event) => handleRoleChange(event, user.id)}
+									class="block w-full mt-2 py-2 px-3 border border-gray-600 bg-gray-800 text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+								>
+									<option value="admin">Admin</option>
+									<option value="user">User</option>
+								</select>
+							</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
 	</div>
+	<BackArrow />
 	<Confirmation
 		isOpen={showConfirmation}
 		message="Are you sure you want to delete {usernameToDelete}?"
